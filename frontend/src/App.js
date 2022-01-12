61f93455714d047fe92bb85262f3c7dcf2357e96
Yuchira Athukorala <yuchira.athukorala@gmail.com>
2022-01-12 17:21:01 +0530

    second commit

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+export default class App extends Component {
+constructor(props){
+  super(props);
+
+  this.state={
+    posts:[]
+  };
+
+}
+
+retrievePosts(){
+  axios.get("http://localhost:8000/posts").then( res =>{
+    if(res.data.success){
+      this.setState({
+        posts:res.data.existingPosts
+      });
+
+      console.log(this.state.posts)
+    }
+  });
+}
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+//import routes
+const postRoutes = require('./routes/posts');
 
-const port = 8006;
-const DB_URL = 'mongodb+srv://yuchira1:yuchira1999@clusteryja.wbn3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//app middileware
+app.use(bodyParser.json());
+app.use(cors());
 
-app.listen(port, () =>{
-    console.log(`App is running on ${port}`);
+//route middleware
+app.use(postRoutes);
+
+
+const PORT = 8006;
+const DB_URL = 'mongodb+srv://yuchira1:yuchira1999@clusteryja.wbn3u.mongodb.net/mernCurd?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL)
+.then(() =>{
+    console.log('DB Connected');
+})
+.catch((err) => console.log('DN Connection error',err));
+
+app.listen(PORT, () =>{
+    console.log(`App is running on ${PORT}`);
 });
